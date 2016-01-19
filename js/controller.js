acc66153321627013f6fd0c30b9b973fb6794f34
Quinn Avila <qavila@drw.com>
2016-01-18 19:51:57 -0600

    working on table features orderby

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,13 @@
 angular.module("myControllers", [])
 .controller("myController", function($scope) {
+  $scope.name = "";
+  $scope.height = "";
+  $scope.weight = "";
+  $scope.begBodyFat = 0;
+  $scope.endBodyFat = 0;
+  $scope.sortType = "name";
+  $scope.sortReverse = false;
+  $scope.searchContestant = "";
   $scope.contestants = [{
     name: "Bob Hope",
     height: "5\'8\"",
@@ -22,6 +30,21 @@ angular.module("myControllers", [])
     endfatPercentage: 0,
     percentageLoss: 0
   }];
-  $scope.test = "Blah!"
+  $scope.test = "2016"
+  $scope.editContestant = function(contestant) {
+    // console.log(contestant.name)
+  }
+  $scope.addContestant = function() {
+    // console.log($scope.name)
+    $scope.contestants.push({
+      name: $scope.name,
+      height: $scope.height,
+      weight: $scope.weight,
+      begfatPercentage: $scope.begBodyFat,
+      endfatPercentage: $scope.endBodyFat
+    })
+  }
+  $scope.deleteContestant = function() {
 
+  }
 })
