7984a6790af602540ff0ce358e2a274d709c033c
Quinn Avila <qavila@drw.com>
2016-01-20 13:15:51 -0600

    edit working

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,7 @@
 angular.module("myControllers", [])
 .controller("myController", function($scope) {
+  // Thinking about how to make this better to allow editing multiple rows
+  $scope.editData = false;
   $scope.name = "";
   $scope.height = "";
   $scope.weight = "";
@@ -8,7 +10,15 @@ angular.module("myControllers", [])
   $scope.sortType = "name";
   $scope.sortReverse = false;
   $scope.searchContestant = "";
+  startCounter = 3
+  $scope.id = function() {
+    startCounter = startCounter+1
+    // console.log(startCounter)
+    return startCounter
+  };
   $scope.contestants = [{
+    id: 1,
+    editData: false,
     name: "Bob Hope",
     height: "5\'8\"",
     weight: 220,
@@ -16,13 +26,17 @@ angular.module("myControllers", [])
     endfatPercentage: 0,
     percentageLoss: 0
   },{
-    name: "Bob Chin",
+    id: 2,
+    editData: false,
+    name: "Albert Einsten",
     height: "5\'6\"",
     weight: 160,
     begfatPercentage: 15,
     endfatPercentage: 0,
     percentageLoss: 0
   },{
+    id: 3,
+    editData: false,
     name: "Batman",
     height: "6\'1\"",
     weight: 225,
@@ -30,13 +44,17 @@ angular.module("myControllers", [])
     endfatPercentage: 0,
     percentageLoss: 0
   }];
-  $scope.test = "2016"
+  $scope.test = "2016";
+
   $scope.editContestant = function(contestant) {
     // console.log(contestant.name)
+    contestant.editData = true
   }
   $scope.addContestant = function() {
-    // console.log($scope.name)
+    // console.log($scope.id())
     $scope.contestants.push({
+      id: $scope.id(),
+      editData: false,
       name: $scope.name,
       height: $scope.height,
       weight: $scope.weight,
@@ -44,7 +62,11 @@ angular.module("myControllers", [])
       endfatPercentage: $scope.endBodyFat
     })
   }
-  $scope.deleteContestant = function() {
-
+  $scope.deleteContestant = function(idx) {
+    // console.log(idx)
+    $scope.contestants.splice(idx, 1)
+  }
+  $scope.cancel = function() {
+    
   }
 })
